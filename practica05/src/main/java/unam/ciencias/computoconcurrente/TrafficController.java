1cc2fb04261ad1efa055c47b05f9d529830d06e5
SamuelEsd <mr_s_p@ciencias.unam.mx>
2019-05-11 21:44:13 -0500

    Ya se implementó el ejercicio 2.2

--- practica05/src/main/java/unam/ciencias/computoconcurrente/TrafficController.java
+++ practica05/src/main/java/unam/ciencias/computoconcurrente/TrafficController.java
@@ -1,54 +1,168 @@
 package unam.ciencias.computoconcurrente;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
+
+
+// EJERCICIO 1
+// // Descomentar para que jale
+// public class TrafficController {
+//     private AtomicBoolean ocupado = new AtomicBoolean(false);
+//     public void enterOnTheLeft() {
+//         synchronized(this){
+//             if (!ocupado.get()) {
+//                 ocupado.set(true);
+//             }
+//             else {
+//                 try {
+//                     while (ocupado.get()){
+//                         wait();   
+//                     }
+//                     ocupado.set(true);
+//                 } catch (InterruptedException ie){
+//                     Thread.currentThread().interrupt();
+//                 }
+//             }
+//         }
+//     }
+//     public void enterOnTheRight() {
+//         synchronized(this){
+//             if (!ocupado.get()) {
+//                 ocupado.set(true);
+//             }
+//             else {
+//                 try {
+//                     while (ocupado.get()){
+//                         wait();   
+//                     }
+//                     ocupado.set(true);
+//                 } catch (InterruptedException ie){
+//                     Thread.currentThread().interrupt();
+//                 }
+//             }
+//         }
+//     }
+//     public void leavingOnTheLeft() {
+//         synchronized(this){
+//             ocupado.set(false);
+//             notify();
+//         }
+//     }
+//     public void leavingOnTheRight() {
+//         synchronized(this){
+//             ocupado.set(false);
+//             notify();
+//         }
+//     }
+// }
+
+
+// ACTIVIDAD 2.1 Prioridad a los carros pasando en el momento
+// public class TrafficController {
+//     private AtomicInteger rojos= new AtomicInteger(0);
+//     private AtomicInteger azules= new AtomicInteger(0);
+
+//     // rojo
+//     public void enterOnTheLeft() {
+//         synchronized(this){
+//             if (azules.get() == 0) {
+//                 rojos.incrementAndGet();
+//             }
+//             else {
+//                 try {
+//                     while (azules.get() > 0){
+//                         wait();   
+//                     }
+//                     rojos.incrementAndGet();
+//                 } catch (InterruptedException ie){
+//                     Thread.currentThread().interrupt();
+//                 }
+//             }
+//         }
+//     }
+//     public void enterOnTheRight() {
+//         synchronized(this){
+//             if (rojos.get() == 0) {
+//                 azules.incrementAndGet();
+//             }
+//             else {
+//                 try {
+//                     while (rojos.get() > 0){
+//                         wait();   
+//                     }
+//                     azules.incrementAndGet();
+//                 } catch (InterruptedException ie){
+//                     Thread.currentThread().interrupt();
+//                 }
+//             }
+//         }
+//     }
+//     public void leavingOnTheLeft() {
+//         synchronized(this){
+//             azules.getAndDecrement();
+//             notifyAll();
+//         }
+//     }
+//     // rojo
+//     public void leavingOnTheRight() {
+//         synchronized(this){
+//             rojos.getAndDecrement();
+//             notifyAll();
+//         }
+//     }
+// }
+
+
 public class TrafficController {
-    private AtomicBoolean ocupado = new AtomicBoolean(false);
+    private AtomicInteger rojos= new AtomicInteger(0);
+    private AtomicInteger azules= new AtomicInteger(0);
+    private AtomicBoolean tocaRojo = new AtomicBoolean(false);
+
+    // rojo
     public void enterOnTheLeft() {
         synchronized(this){
-            if (!ocupado.get()) {
-                ocupado.set(true);
-            }
-            else {
-                try {
-                    while (ocupado.get()){
-                        wait();   
-                    }
-                    ocupado.set(true);
-                } catch (InterruptedException ie){
-                    Thread.currentThread().interrupt();
+            try {
+                while (!tocaRojo.get() || rojos.get() != 0){
+                    wait();   
                 }
+                rojos.incrementAndGet();
+            } catch (InterruptedException ie){
+                Thread.currentThread().interrupt();
             }
         }
     }
     public void enterOnTheRight() {
         synchronized(this){
-            if (!ocupado.get()) {
-                ocupado.set(true);
-            }
-            else {
-                try {
-                    while (ocupado.get()){
-                        wait();   
-                    }
-                    ocupado.set(true);
-                } catch (InterruptedException ie){
-                    Thread.currentThread().interrupt();
+            try {
+                while (tocaRojo.get() || azules.get() != 0){
+                    wait();   
                 }
+                azules.incrementAndGet();
+            } catch (InterruptedException ie){
+                Thread.currentThread().interrupt();
             }
         }
     }
     public void leavingOnTheLeft() {
         synchronized(this){
-            ocupado.set(false);
-            notify();
+            tocaRojo.set(!tocaRojo.get());
+            if (rojos.get() == 1){
+                rojos.decrementAndGet();
+            }
+            notifyAll();
         }
     }
+    // rojo
     public void leavingOnTheRight() {
         synchronized(this){
-            ocupado.set(false);
-            notify();
+            tocaRojo.set(!tocaRojo.get());
+            if (azules.get() == 1){
+                azules.decrementAndGet();
+            }
+            notifyAll();
         }
     }
 }
